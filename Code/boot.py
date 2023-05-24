e7f09f0327bea64172c6b26c9a6ae53fb2a30fa2
Alden Dent <55511021+adent11@users.noreply.github.com>
2023-05-24 11:09:16 -0400

    Update boot.py

--- Code/boot.py
+++ Code/boot.py
@@ -1,18 +1,21 @@
+# WindCapstone boot code
+# Alden Dent
+
 import board, digitalio, storage, time
 
-led = digitalio.DigitalInOut(board.LED)
+led = digitalio.DigitalInOut(board.LED) # LED setup
 led.direction = digitalio.Direction.OUTPUT
 led.value = False
 
-buttonPin = digitalio.DigitalInOut(board.GP0)
+buttonPin = digitalio.DigitalInOut(board.GP0) # Button Setup
 buttonPin.direction = digitalio.Direction.INPUT
 buttonPin.pull = digitalio.Pull.UP
 
-# If write pin is connected to ground on start-up, CircuitPython can write to CIRCUITPY filesystem.
+# If button is pressed, CircuitPython can write to CIRCUITPY filesystem.
 if not buttonPin.value:
-    led.value = True
+    led.value = True # Turns on LED ot show that Pico is in write mode
     time.sleep(3)
     storage.remount("/", readonly=False)
 
-while not buttonPin.value:
+while not buttonPin.value: # Waits for button release to continue
     pass

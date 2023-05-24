765ba1a8d91ce00c6a7d03103087f8704b572b53
Alden Dent <55511021+adent11@users.noreply.github.com>
2023-05-24 10:59:42 -0400

    Update code.py

--- Code/code.py
+++ Code/code.py
@@ -1,37 +1,40 @@
+# WindCapstone main code
+# Alden Dent
+
 import time, board, digitalio, analogio, adafruit_mpl3115a2, busio, os
 
-i2c = busio.I2C(board.GP17, board.GP16)
+i2c = busio.I2C(board.GP17, board.GP16) # Barometric altimeter setup
 b = adafruit_mpl3115a2.MPL3115A2(i2c)
 b.sealevel_pressure = 103040
 
-windIn = analogio.AnalogIn(board.GP28)
+windIn = analogio.AnalogIn(board.GP28) # Wind sensor setup
 tempIn = analogio.AnalogIn(board.GP27)
 
-buttonPin = digitalio.DigitalInOut(board.GP0)
+buttonPin = digitalio.DigitalInOut(board.GP0) # Mode button setup
 buttonPin.direction = digitalio.Direction.INPUT
 buttonPin.pull = digitalio.Pull.UP
 
-rPin = digitalio.DigitalInOut(board.GP6)
+rPin = digitalio.DigitalInOut(board.GP6) #LED setup
 rPin.direction = digitalio.Direction.OUTPUT
 gPin = digitalio.DigitalInOut(board.GP7)
 gPin.direction = digitalio.Direction.OUTPUT
 bPin = digitalio.DigitalInOut(board.GP8)
 bPin.direction = digitalio.Direction.OUTPUT
 
-red, green, blue, cyan = (1, 0, 0), (0, 1, 0), (0, 0, 1), (0, 1, 1)
+red, green, blue, cyan = (1, 0, 0), (0, 1, 0), (0, 0, 1), (0, 1, 1) # variables for different colors to input to setColor function
 
-def setColor(c):
+def setColor(c): # Sets the color of the LED
     rPin.value = c[0]
     gPin.value = c[1]
     bPin.value = c[2]
 
-def buttonPressed():
+def buttonPressed(): # Simplifying confusing negatives for semantic ease
     return not buttonPin.value
 
-def getVoltage(inVal):
+def getVoltage(inVal): # Converts analog input to voltage
     return (inVal * 3.3) / 65536
 
-def voltsToArduinoIn(volts):
+def voltsToArduinoIn(volts): # Converts voltage input to arduino analog reading
     return volts * 1023 / 5
 
 flightNum = 1
@@ -58,12 +61,12 @@ try:
             pass
         time.sleep(.5)
         setColor(cyan)
-        with open(filePath, "w") as file:
+        with open(filePath, "w") as file: # Writes data to a file
             file.write("altitude,windMPH,barometerTemp,windTemp,windVolts,timeFromStart\n")
             for dataLine in dataList:
                 file.write(dataLine)
         flightNum = flightNum + 1
-except:
+except: # Shows if there has been an error
     setColor(red)
     while True:
         pass
